e39de6dfa5836815ea9617109cc0c7f4af2c11cf
bhgt <bhagat.nagi@gmail.com>
2020-01-25 21:28:32 +0530

    Tests 1 & 2 on posting

---
+++ test2.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var multer = require('multer');
+var ejs = require('ejs');
+var path = require('path');
+
+const app = express();
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
