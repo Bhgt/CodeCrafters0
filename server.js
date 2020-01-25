ffb7865446ba12b1ec79e9fca9df0d5f2667e400
bhgt <bhagat.nagi@gmail.com>
2020-01-25 15:12:44 +0530

    changes to server.js

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
-var http = require('http');
-var fs = require('fs');
+var express = require("express");
 
-var server = http.createServer(function(req, res){
-	
-	var file = '/index.html'
-	var contentType = 'text/html'
-	
-	if(req.url==='/' || req.url==='/home'){
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		fs.createReadStream(__dirname+'/index.html', 'utf-8').pipe(res);
-	}else{
-		res.writeHead(404, {'Content-Type': 'text/plain'});
-		res.end('Error 404: Not Found');
-	}
-	
+var app = express();
+
+app.get("/", function(req, res){
+	res.send("Hello Express");
 });
 
-server.listen(3000, '127.0.0.1');
-console.log('Server listening to port 3000');
+app.get("/say/:word", function(req, res){
+	console.log("Saying "+req.params.word);
+	res.send(" "+req.params.word);
+});
+
+app.listen(3000);
